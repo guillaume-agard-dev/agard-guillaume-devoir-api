9112ce5acbd59926203e240d9c5f81060fa46a06
Guillaume Agard <guillaumeagard.dev@gmail.com>
2025-08-14 12:53:40 +0200

    correction orthographe

--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
-const userShema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
@@ -21,7 +21,7 @@ const userShema = new mongoose.Schema({
 });
 
 
-userShema.pre("save", async function (next) {
+userSchema.pre("save", async function (next) {
     if (!this.isModified("password")) return next();
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
