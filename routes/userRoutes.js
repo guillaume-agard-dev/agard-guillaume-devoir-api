ded8a4cffad353a1dbca58e4e4c3b857807e60c4
Guillaume Agard <guillaumeagard.dev@gmail.com>
2025-08-14 09:17:25 +0200

    Authentification

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,27 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
+const auth = require("../middleware/auth");
 
 const router = express.Router();
+
+// Register
+router.post("/register", async (req, res) => {
+  const { username, email, password } = req.body;
+
+  try {
+    const userExists = await User.findOne({ email });
+    if (userExists) return res.status(400).json({ message: "Email déjà utilisé" });
+
+    const user = new User({ username, email, password });
+    await user.save();
+
+    res.status(201).json({ message: "Utilisateur créé avec succès" });
+  } catch (err) {
+    res.status(500).json({ message: "Erreur serveur" });
+  }
+})
+
 
 // Login
 router.post("/login", async (req, res) => {
@@ -27,3 +46,10 @@ router.post("/login", async (req, res) => {
     res.status(500).json({ message: "Erreur serveur" });
   }
 });
+
+// Logout
+router.get("/logout", auth, (req, res) => {
+  res.json({ message: "Déconnexion réussie" });
+});
+
+module.exports = router;
